b408d07c3e383c72496f564a4d9354ab9fef8cc5
Edward Davies <morgandavies428@gmail.com>
2025-04-08 17:26:19 +0100

    Writen register file and basic testbench

---
+++ register32I_stim.sv
@@ -0,0 +1,60 @@
+// RISCV32I register file module testbench
+module register32I_stim;
+
+timeunit 1ns; timeprecision 10ps;
+
+parameter CLOCK_P = 100;
+
+logic reset, clock;
+// Intial reset
+initial begin reset = 0; #10 reset = 1; #10 reset = 0; end
+
+// Clock signal
+always begin clock = 0; #(CLOCK_P/2) clock = 1; #(CLOCK_P/2) clock = 0; end
+
+parameter dataW = 32;
+
+// inputs
+logic RegWriteControl;
+logic [dataW-1:0] RegDataIn, RegData1, RegData2, RegWriteAddr;
+
+// outputs
+logic [dataW-1:0] RegDataOut1, RegDataOut2;
+
+registerR32I #(dataW) reg1
+(
+    .clock(clock),
+    .reset(reset),
+    .RegWriteControl(RegWriteControl),
+    .RegDataIn(RegDataIn),
+    .RegData1(RegData1),
+    .RegData2(RegData2),
+    .RegWriteAddr(RegWriteAddr),
+    .RegDataOut1(RegDataOut1),
+    .RegDataOut2(RegDataOut2)
+);
+
+initial
+begin
+    // Setup
+    RegDataIn = 0;
+    RegWriteAddr = 0;
+    RegWriteControl = 0;
+    RegData1 = 0;
+    RegData2 = 0;
+    #CLOCK_P
+    RegWriteAddr = 1;
+    RegDataIn = 897;
+    #CLOCK_P
+    RegData1 = 1;
+    RegData2 = 2;
+    RegWriteAddr = 2;
+    RegDataIn = 666;
+    #CLOCK_P
+    RegData1 = 0;
+    RegWriteAddr = 0;
+    #(CLOCK_P*2)
+    $finish;$stop;
+end
+
+endmodule
---
+++ registerR32I.sv
@@ -0,0 +1,45 @@
+// RISCV32I register file module
+// implements 32 32-bit general registers
+module registerR32I #(parameter dataW = 32)
+(
+    input logic clock, reset,
+    input logic RegWriteControl,                                        // Writeback control signal
+    input logic [dataW-1:0] RegDataIn,                                  // RegWriteControlback data input
+    input logic [$clog2(dataW)-1:0] RegData1, RegData2,                 // Register read index
+    input logic [$clog2(dataW)-1:0] RegWriteAddr,                           // Writeback register index
+    output logic [dataW-1:0] RegDataOut1, RegDataOut2
+);
+
+timeunit 1ns; timeprecision 10ps;
+
+// first register is zero reg, all other registers are general purpose
+logic [dataW-1:0] RegisterFile [31:1];
+
+always_comb
+begin
+    case (RegData1)
+        0:          RegDataOut1 = 0;
+        default:    RegDataOut1 = RegisterFile[RegData1];
+    endcase
+
+    case (RegData2)
+        0:          RegDataOut2 = 0;
+        default:    RegDataOut2 = RegisterFile[RegData2];
+    endcase
+end
+
+always_ff @(posedge clock, posedge reset)
+begin
+    if (reset)
+    begin
+        RegisterFile <= '{default: '0};
+    end
+    else
+    begin
+        // Writes are made to register at RegWriteAddr, ban writes to x0 for hardware saftey
+        if (RegWriteControl && RegWriteAddr != 0) RegisterFile[RegWriteAddr] <= RegDataIn;
+    end
+end
+
+
+endmodule
