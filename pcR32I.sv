05c4cdd8f2179b8d92c435ace0ab7b5b494286b6
Edward Davies <morgandavies428@gmail.com>
2025-04-12 22:28:07 +0100

    Fixed issue with BranchStatus length

--- pcR32I.sv
+++ pcR32I.sv
@@ -4,7 +4,7 @@
 module pcR32I #(parameter dataW = 32)
 (
     input logic clock, reset,
-    input wire EQ, NE, LT, LTU, GE, GEU,           // Conditional input flags
+    input logic EQ, NE, LT, LTU, GE, GEU,          // Conditional input flags
     input logic BranchControl,                     // High if PC should be branching
     input logic [2:0] PCBranchType,                // indicates type of branch to take, see branchcodes.sv
     input logic signed [dataW-1:0] BranchOffset,   // offset to add to the PC if PCBranch is high
@@ -13,8 +13,8 @@ module pcR32I #(parameter dataW = 32)
 
 timeunit 1ns; timeprecision 10ps;
 
-wire [2:0] BranchStatus;
-assign BranchStatus = {2'd0, GEU, GE, LTU, LT, NE, EQ};
+wire [5:0] BranchStatus;
+assign BranchStatus = {GEU, GE, LTU, LT, NE, EQ};
 
 always_ff @( posedge clock, posedge reset )
 begin
