3efda6b0df576e544b7bd021a2d7e0fb40863adf
Edward Davies <morgandavies428@gmail.com>
2025-04-27 21:28:59 +0100

    naming fix

--- signExtender.sv
+++ signExtender.sv
@@ -2,8 +2,7 @@
 module signExtender
 (
     input logic sign,
-    output logic [19:0] IStypeSign,     // 20 bit wide sign
-    output logic [18:0] BTypeSign,      // 19 bit wide sign
+    output logic [19:0] ISBtypeSign,     // 20 bit wide sign
     output logic [10:0] JTypeSign       // 11 bit wide sign
 );
 
